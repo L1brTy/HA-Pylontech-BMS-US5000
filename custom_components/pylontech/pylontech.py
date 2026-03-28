a7c76315eb69a60cd20a3eab12ded994c1e24d14
L1brTy <57085514+L1brTy@users.noreply.github.com>
2026-03-28 22:16:01 +0100

    Update pylontech.py

--- custom_components/pylontech/pylontech.py
+++ custom_components/pylontech/pylontech.py
@@ -1,3 +1,4 @@
+"""Parser for Pylontech US5000 Console Output."""
 from __future__ import annotations
 
 class BatValue:
@@ -9,14 +10,15 @@ class BatValue:
         self.curr = int(p[2]) / 1000.0
         self.tempr = int(p[3]) / 1000.0
         self.base_state = p[4]
+        # In der 'bat' Tabelle steht SOC an Index 8
         self.soc = int(p[8].replace('%', '')) if '%' in p[8] else 0
-        self.balance = p[-1]
+        self.balance = p[-1] # BAL ist die letzte Spalte
 
 class BatCommand:
     def __init__(self, lines: tuple[str]):
         self.values = []
         for line in lines:
-            if line and line[0].isdigit():
+            if line and line[0].isdigit() and len(line.split()) > 10:
                 try: self.values.append(BatValue(line))
                 except: continue
 
@@ -35,16 +37,20 @@ class PwrCommand:
         target = str(pack_id)
         for line in lines:
             p = line.split()
+            # Deine US5000: Index 1 & 2. Volt=Index 1, SOC=Index 12
             if len(p) > 12 and p[0] == target and p[8] != "Absent":
-                self.volt.value = int(p[1]) / 1000.0
-                self.curr.value = int(p[2]) / 1000.0
-                self.temp.value = int(p[3]) / 1000.0
-                self.cell_temp_low.value = int(p[4]) / 1000.0
-                self.cell_temp_high.value = int(p[5]) / 1000.0
-                self.cell_volt_low.value = int(p[6]) / 1000.0
-                self.cell_volt_high.value = int(p[7]) / 1000.0
-                self.base_state.value = p[8]
-                self.soc.value = int(p[12].replace('%', '')) if '%' in p[12] else 0
+                try:
+                    self.volt.value = int(p[1]) / 1000.0
+                    self.curr.value = int(p[2]) / 1000.0
+                    self.temp.value = int(p[3]) / 1000.0
+                    self.cell_temp_low.value = int(p[4]) / 1000.0
+                    self.cell_temp_high.value = int(p[5]) / 1000.0
+                    self.cell_volt_low.value = int(p[6]) / 1000.0
+                    self.cell_volt_high.value = int(p[7]) / 1000.0
+                    self.base_state.value = p[8]
+                    # SOC steht in der 'pwr' Zeile an Index 12
+                    self.soc.value = int(p[12].replace('%', '')) if '%' in p[12] else 0
+                except: pass
                 break
 
 class StatCommand:
@@ -52,7 +58,8 @@ class StatCommand:
         self.cycle_count = 0
         for line in lines:
             if "CYCLE Times" in line:
-                try: self.cycle_count = int(line.split(":")[-1].strip())
+                try:
+                    self.cycle_count = int(line.split(":")[-1].strip())
                 except: pass
 
 class InfoCommand:
